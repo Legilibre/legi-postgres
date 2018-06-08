25abb991068a8c48d95c56c82d64eea81a4f2b21
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-06-08 04:00:07 +0200

    always create indexes

--- scripts/pgloader-after.sql
+++ scripts/pgloader-after.sql
@@ -1,18 +1,18 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'readaccess') THEN
-      CREATE INDEX textes_versions_cid_idx ON "public".textes_versions (cid);
-      CREATE INDEX textes_versions_date_debut_idx ON "public".textes_versions (date_debut);
-      CREATE INDEX textes_versions_date_fin_idx ON "public".textes_versions (date_fin);
-      CREATE INDEX sommaires_debut_idx ON "public".sommaires (debut);
-      CREATE INDEX sommaires_fin_idx ON "public".sommaires (fin);
-      CREATE INDEX articles_id_idx ON "public".articles (id);
-      CREATE INDEX articles_cid_idx ON "public".articles (cid);
-      CREATE INDEX articles_date_debut_idx ON "public".articles (date_debut);
-      CREATE INDEX articles_date_fin_idx ON "public".articles (date_fin);
-      CREATE INDEX articles_section_idx ON "public".articles (section);
-      CREATE INDEX articles_num_idx ON "public".articles (num);
+    CREATE INDEX textes_versions_cid_idx ON "public".textes_versions (cid);
+    CREATE INDEX textes_versions_date_debut_idx ON "public".textes_versions (date_debut);
+    CREATE INDEX textes_versions_date_fin_idx ON "public".textes_versions (date_fin);
+    CREATE INDEX sommaires_debut_idx ON "public".sommaires (debut);
+    CREATE INDEX sommaires_fin_idx ON "public".sommaires (fin);
+    CREATE INDEX articles_id_idx ON "public".articles (id);
+    CREATE INDEX articles_cid_idx ON "public".articles (cid);
+    CREATE INDEX articles_date_debut_idx ON "public".articles (date_debut);
+    CREATE INDEX articles_date_fin_idx ON "public".articles (date_fin);
+    CREATE INDEX articles_section_idx ON "public".articles (section);
+    CREATE INDEX articles_num_idx ON "public".articles (num);
 
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'readaccess') THEN
       DROP ROLE IF EXISTS readaccess;
       CREATE ROLE readaccess;
       GRANT USAGE ON SCHEMA public TO readaccess;
